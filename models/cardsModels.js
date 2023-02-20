709c47604af29f1095684cf0b87aea519058a8a0
MarianaCGoncalves <marii03.goncalves@gmail.com>
2023-02-20 17:00:37 +0000

    added filter by type and lore

--- models/cardsModels.js
+++ models/cardsModels.js
@@ -35,6 +35,78 @@ const pool = require("../config/database");
                       return {status: 500, result: err };
                   }
               }
-          }
+
+              static async getById(id) {
+                try {
+                    let dbResult = 
+                        await pool.query("Select * from cards where crd_id=$1",[id]);
+                        let dbCards = dbResult.rows;
+                    if (!dbCards.length)
+                        return {status:404, result: {msg: "No card found with that identifier"}};
+                    let dbCard = dbCards[0];
+                    let result = cardFromDB(dbCard);
+                    return {status: 200, result: result};
+                } catch (err) {
+                    console.log(err);
+                    return {status: 500, result: err };
+                }
+            }
           
+          
+          static async save(newCard) {
+            try {
+                let dbResult =
+                    await pool.query("Select * from cards where crd_name=$1", [newCard.name]);
+                    let dbCards = dbResult.rows;
+                if (dbCards.length!=0)
+                    return {
+                        status: 400, result: [{
+                            location: "body", param: "name",
+                            msg: "That name already exists"
+                        }]
+                    };
+                let [result] =
+                    await pool.query(`Insert into cards (crd_name, crd_img_url, crd_lore, 
+                    crd_description, crd_level, crd_cost, crd_timeout, crd_max_usage, crd_type)
+                    values ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING crd_id`, [newCard.name, newCard.url, newCard.lore,
+                    newCard.description, newCard.level, newCard.cost, newCard.timeout,
+                    newCard.maxUsage, newCard.type]);
+                return { status: 200, result: {msg: "Inserted ", id: result.rows[0].crd_id} };
+            } catch (err) {
+                console.log(err);
+                return { status: 500, result: err };
+            }
+        }
+        
+        static async filterByType(typeId) {
+            try {
+                let result = [];
+                let dbResult =
+                    await pool.query("Select * from cards where crd_type=$1", [typeId]);
+                    let dbCards = dbResult.rows;
+                for (let dbCard of dbCards) {
+                    result.push(cardFromDB(dbCard));
+                }
+                return { status: 200, result: result };
+            } catch (err) {
+                console.log(err);
+                return { status: 500, result: err };
+            }
+        }
+
+        static async filterByLoreOrDescription(text){
+            try{
+                let result= [];
+                let dbResults = 
+                    await pool.query("Select * from cards where crd_description LIKE $1 or crd_lore LIKE $1", ['%'+text+'%']);
+                let dbCards = dbResults.rows;
+                for(let dbCard of dbCards){
+                    result.push(cardFromDB(dbCard));
+                }return {status: 200, result: result};
+            }catch(err){
+                console.log(err);
+                return { status: 500, result: err};
+            }
+        }
+    }
           module.exports = Card;
